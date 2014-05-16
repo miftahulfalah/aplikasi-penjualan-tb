86e74ae5256ed31573f6429d1aed42908de4726b
miftahulfalah <miftahulfalah32@yahoo.com>
2014-05-16 14:55:53 +0700

    updating

--- aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
@@ -1,13 +1,19 @@
 package net.smktarunabhakti.penjualan;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+   private static ApplicationContext appCtx;
+   
+   public static void main(String[] args) {
+	   appCtx = new ClassPathXmlApplicationContext(
+			   "classpath:net/smktarunabhakti/penjualan/" 
+					   + "applicationContex.xml");
+   }
 }
